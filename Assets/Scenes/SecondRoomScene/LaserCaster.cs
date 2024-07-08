5973bd17d6240adf93b366ec66eda7f4b36ca96b
Sébastien Lamard <slamard@norsys.fr>
2024-07-08 17:04:53 +0200

    feat(second-room): add laser

---
+++ Assets/Scenes/SecondRoomScene/LaserCaster.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserCaster : MonoBehaviour
+{
+    private const float MaxLaserDistance = 500;
+    private RaycastHit _raycastHit;
+
+    void Update()
+    {
+        LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
+        if (!lineRenderer) {
+            return;
+        }
+
+        if(Physics.Raycast(transform.position, transform.forward, out _raycastHit))
+        {
+            lineRenderer.SetPosition(1, transform.InverseTransformPoint(_raycastHit.point));
+        } else {
+            lineRenderer.SetPosition(1, transform.position + transform.forward * MaxLaserDistance);
+        }
+    }
+}
