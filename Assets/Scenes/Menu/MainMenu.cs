fcb79b3dd87a65aa9b943f3d3b8746956ff55210
RemiLecas <116066293+RemiLecas@users.noreply.github.com>
2024-07-09 11:45:12 +0200

    Menu (#2)

---
+++ Assets/Scenes/Menu/MainMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+     public Canvas canvasToShow;
+     
+    public void AfficheMenu(){
+        canvasToShow.gameObject.SetActive(true);
+    }
+}
